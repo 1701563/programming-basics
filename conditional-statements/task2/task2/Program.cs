1e1bbbb8d1a480cde1ff372bdc8ac7eede3d5412
Anssi Välimaa <1701563@co.saimia.fi>
2017-09-22 13:18:33 +0300

    siirto

---
+++ conditional-statements/task2/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditional_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa onko numero parillinen vai pariton");
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+            if (evaluatedNumber % 2 == 0) Console.WriteLine($"Numero {evaluatedNumber} on parillinen");
+            else if (evaluatedNumber % 2 != 0) Console.WriteLine($"Numero {evaluatedNumber} on pariton");
+            Console.ReadKey();
+
+        }
+    }
+}
