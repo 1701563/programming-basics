2449532dbdece499ec010d5bab53a87609cc645c
Anssi Välimaa <1701563@co.saimia.fi>
2017-09-22 14:44:59 +0300

    toimii

--- conditional-statements/task3/task3/Program.cs
+++ conditional-statements/task3/task3/Program.cs
@@ -10,6 +10,20 @@ namespace task3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ohjelma tulostaa onko numero positiivinen ja parillinen, positiivinen ja pariton, negatiivinen ja parillinen, negatiivinen ja pariton vai nolla");
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+            
+            if (evaluatedNumber > 0 && evaluatedNumber % 2 == 0) Console.WriteLine($"Numero {evaluatedNumber} on positiivinen ja parillinen");
+            if (evaluatedNumber > 0 && evaluatedNumber % 2 != 0) Console.WriteLine($"Numero {evaluatedNumber} on posiitivinen ja pariton");
+            if (evaluatedNumber < 0 && evaluatedNumber % 2 == 0) Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen ja parillinen");
+            if (evaluatedNumber < 0 && evaluatedNumber % 2 != 0) Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen ja pariton");
+            if (evaluatedNumber == 0) Console.WriteLine("Numero on nolla");
+            Console.ReadKey();
+
         }
     }
 }
